1814cb5ed51944cd04d794dbe346b8000c6ea01e
vainicius <comercial.viniciusgabriel@gmail.com>
2023-05-06 19:49:21 -0300

    MarcaService - desativarMarca OK

--- src/main/java/br/com/uniamerica/estacionamento/service/MarcaService.java
+++ src/main/java/br/com/uniamerica/estacionamento/service/MarcaService.java
@@ -3,7 +3,9 @@ package br.com.uniamerica.estacionamento.service;
 import br.com.uniamerica.estacionamento.entity.Marca;
 import br.com.uniamerica.estacionamento.entity.Modelo;
 import br.com.uniamerica.estacionamento.repository.MarcaRepository;
+import br.com.uniamerica.estacionamento.repository.ModeloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -14,6 +16,8 @@ import java.util.List;
 public class MarcaService {
     @Autowired
     MarcaRepository marcaRepository;
+    @Autowired
+    ModeloRepository modeloRepository;
 
     public Marca cadastrar(final Marca marca){
         Assert.notNull(marca.getNomeMarca(), "O campo 'nome' não pode ser nulo!");
@@ -36,5 +40,17 @@ public class MarcaService {
         return this.marcaRepository.save(marca);
 
     }
+    public ResponseEntity<?> desativarMarca(Long id) {
+        final Marca marcaBanco = this.marcaRepository.findById(id).orElse(null);
+        Assert.notNull(marcaBanco, "Marca não localizada!");
+        if (!this.modeloRepository.findByMarcaId(id).isEmpty()) {
+            marcaBanco.setAtivo(false); //Desativando uma marca se ela estiver relacionada à um Modelo.
+            this.marcaRepository.save(marcaBanco);
+            return ResponseEntity.ok("Marca desativada.");
+        } else {
+            this.marcaRepository.delete(marcaBanco);
+            return ResponseEntity.ok("Marca deletada.");
+        }
 
+    }
 }
