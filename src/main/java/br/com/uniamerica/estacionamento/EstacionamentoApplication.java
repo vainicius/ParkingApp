9f9382c53b6ef2863c83e65bca041c84c3b9d3dd
vainicius <comercial.viniciusgabriel@gmail.com>
2023-06-24 23:24:02 -0300

    CORS Error fixed!

--- src/main/java/br/com/uniamerica/estacionamento/EstacionamentoApplication.java
+++ src/main/java/br/com/uniamerica/estacionamento/EstacionamentoApplication.java
@@ -2,6 +2,7 @@ package br.com.uniamerica.estacionamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @SpringBootApplication
 public class EstacionamentoApplication {
@@ -9,5 +10,11 @@ public class EstacionamentoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EstacionamentoApplication.class, args);
 	}
-
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3000")
+				.allowedMethods("GET", "POST", "PUT", "DELETE")
+				.allowedHeaders("Content-Type")
+				.allowCredentials(true);
+	}
 }
